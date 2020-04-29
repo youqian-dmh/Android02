4de9fcda9a53f0d13864093d86b440985198aabd
youqian-dmh <605736963@qq.com>
2020-04-29 11:30:44 +0800

    提交4.29

---
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180201/MyOpenHelper.java
@@ -0,0 +1,33 @@
+package cn.edu.sdwu.android02.classroom.sn170507180201;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by ASUS on 2020/4/29.
+ */
+
+public class MyOpenHelper extends SQLiteOpenHelper {
+    private String STUDENT_TB_SQL="create table student(id intger primary key autoincrement,stuname text,stutel text)";
+    public MyOpenHelper(Context context){
+        //Context context上下文，String name数据库名称,CursorFactory factory传入NULL，int version数据库的版本
+        super(context,"stud.db",null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        //当打开数据库时，发现数据库并不存在，此时会自动创建数据库，然后调用onCreat方法
+        //在本方法，完成数据库对象（表，视图，索引等）的创建
+        //通过SQLiteDatabase对象，完成SQL执行
+        sqLiteDatabase.execSQL(STUDENT_TB_SQL);
+        Log.i(MyOpenHelper.class.toString(),"onCreate");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
